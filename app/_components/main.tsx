a3e4473b6d3a6dff163280130fee8abfe8fe58a4
dhruv-kaushik <dhrvkaushikk@gmail.com>
2024-05-30 23:18:08 +0530

    format Code (make page server Component)

---
+++ app/_components/main.tsx
@@ -0,0 +1,50 @@
+"use client";
+import JSZip from "jszip";
+import { useState } from "react";
+import { DataTable } from "./data-table";
+import { Input } from "@/components/ui/input";
+import { PackData, columns } from "./columns";
+
+export function MainPackOverlay() {
+  const [data, setData] = useState<PackData[]>([]);
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = async function () {
+      const zip = new JSZip();
+      const content = await zip.loadAsync(reader.result as ArrayBuffer);
+      const packDataPromises = Object.values(content.files)
+        .filter((file) => file.name.endsWith(".png") && !file.dir)
+        .map((file) =>
+          file.async("base64").then((data) => {
+            const blob = new Blob([Buffer.from(data, "base64")], {
+              type: "image/png",
+            });
+            const fileName =
+              file.name.split("/").pop()?.replace(".png", "") || "";
+            return {
+              pack_name: fileName,
+              pack_location: file.name,
+              pack_file: blob,
+            };
+          })
+        );
+      const packData = await Promise.all(packDataPromises);
+      setData(packData);
+    };
+    reader.readAsArrayBuffer(file);
+  };
+
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <div className="flex flex-row items-center justify-center space-x-4">
+        <Input type="file" onChange={handleFileChange} />
+      </div>
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={data} />
+      </div>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,8 @@
-"use client";
-import React, { useState } from "react";
-import JSZip from "jszip";
-import { Input } from "@/components/ui/input";
-import { PackData, columns } from "./_components/columns";
-import { DataTable } from "./_components/data-table";
-
+import { MainPackOverlay } from "./_components/main";
 export default function Home() {
-  const [data, setData] = useState<PackData[]>([]);
-  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (!file) return;
-
-    const reader = new FileReader();
-    reader.onload = async function () {
-      const zip = new JSZip();
-      const content = await zip.loadAsync(reader.result as ArrayBuffer);
-      const packDataPromises = Object.values(content.files)
-        .filter((file) => file.name.endsWith(".png") && !file.dir)
-        .map((file) =>
-          file.async("base64").then((data) => {
-            const blob = new Blob([Buffer.from(data, "base64")], {
-              type: "image/png",
-            });
-            const fileName =
-              file.name.split("/").pop()?.replace(".png", "") || "";
-            return {
-              pack_name: fileName,
-              pack_location: file.name,
-              pack_file: blob,
-            };
-          })
-        );
-      const packData = await Promise.all(packDataPromises);
-      setData(packData);
-    };
-    reader.readAsArrayBuffer(file);
-  };
-
   return (
-    <main className="flex flex-col items-center justify-center">
-      <div className="flex flex-row items-center justify-center space-x-4">
-        <Input type="file" onChange={handleFileChange} />
-      </div>
-      <div className="container mx-auto py-10">
-        <DataTable columns={columns} data={data} />
-      </div>
-    </main>
+    <>
+      <MainPackOverlay />
+    </>
   );
 }
