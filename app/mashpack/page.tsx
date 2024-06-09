5d01781ea75f13068f03e84c9694cd0e397c7c3e
dhruv-kaushik <dhrvkaushikk@gmail.com>
2024-06-09 23:53:09 +0530

    nothin basically

--- app/mashpack/page.tsx
+++ app/mashpack/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 import { Input } from "@/components/ui/input";
 import JSZip from "jszip";
+import { ArrowRightSquareIcon } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 
 export default function Page() {
   const [packs, setPacks] = useState<
-    { file: File; mcMeta: string; packPng: Blob }[]
+    { fileName: string; data: JSZip; mcMeta: string; packPng: Blob }[]
   >([]);
   const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
+    console.log(files);
     if (!files) return;
 
     const newPacks = [];
@@ -20,12 +22,14 @@ export default function Page() {
       }
       const zip = new JSZip();
       const data = await zip.loadAsync(file);
-
+      const fileName = await file.name;
       const mcMeta = await data.file("pack.mcmeta")?.async("text");
       const packPng = await data.file("pack.png")?.async("blob");
+      if (!fileName) return alert("Invalid Texture PAck");
       if (!mcMeta) return alert("Invalid Texture PAck");
+      if (!data) return alert("Invalid Texture PAck");
       if (!packPng) return alert("Invalid Texture PAck");
-      newPacks.push({ file, mcMeta, packPng });
+      newPacks.push({ fileName, data, mcMeta, packPng });
     }
 
     setPacks(newPacks);
@@ -33,42 +37,45 @@ export default function Page() {
   return (
     <div>
       <Input type="file" multiple onChange={handleFileChange} />
-      {packs.map((pack, i) => (
-        <div key={i}>
-          {(() => {
-            try {
-              const mcMetaWithoutBOM = pack.mcMeta.replace(/^\uFEFF/, "");
-              const mcMetaWithoutControlChars = mcMetaWithoutBOM.replace(
-                /[\x00-\x1F\x7F-\x9F]/g,
-                ""
-              );
-              const parsedJson = JSON.parse(mcMetaWithoutControlChars);
-              return (
-                <div className="grid grid-cols-3 p-2">
-                  <div className="bg-black text-white flex items-center">
-                    <Image
-                      src={URL.createObjectURL(pack.packPng)}
-                      alt={pack.file.name}
-                      width={96}
-                      height={96}
-                      className="object-cover mr-3"
-                    />
-                    <div>
-                      <h2 className="mt-2 text-xl">{pack.file.name}</h2>
-                      <p className="text-sm">
-                        {parsedJson.pack.description.replace(/§./g, "")}
-                      </p>
+      <div>
+        {packs.map((pack, i) => (
+          <div key={i}>
+            {(() => {
+              try {
+                const mcMetaWithoutBOM = pack.mcMeta.replace(/^\uFEFF/, "");
+                const mcMetaWithoutControlChars = mcMetaWithoutBOM.replace(
+                  /[\x00-\x1F\x7F-\x9F]/g,
+                  ""
+                );
+                const parsedJson = JSON.parse(mcMetaWithoutControlChars);
+                return (
+                  <div className="grid grid-cols-3 p-2">
+                    <div className="bg-black text-white flex items-center">
+                      <Image
+                        src={URL.createObjectURL(pack.packPng)}
+                        alt={pack.fileName}
+                        width={96}
+                        height={96}
+                        className="object-cover mr-3"
+                      />
+                      <div>
+                        <h2 className="mt-2 text-xl">{pack.fileName}</h2>
+                        <p className="text-sm">
+                          {parsedJson.pack.description.replace(/§./g, "")}
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            } catch (e) {
-              alert(e);
-              return `Invalid JSON: ${pack.mcMeta}`;
-            }
-          })()}
-        </div>
-      ))}
+                );
+              } catch (e) {
+                alert(e);
+                return `Invalid JSON: ${pack.mcMeta}`;
+              }
+            })()}
+          </div>
+        ))}
+        <ArrowRightSquareIcon className="w-10 h-10" />
+      </div>
     </div>
   );
 }
